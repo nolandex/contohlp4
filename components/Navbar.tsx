ba190354b18ccb6c18a8ab358702373358c70a62
nolandex <stemapal1234@gmail.com>
2025-06-21 00:30:15 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,40 +5,35 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Popover } from "@headlessui/react";
 import { Menu, X } from "lucide-react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import data from "../../config/data.json";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const headerLinks = [
-    { label: "About us", href: "/" },
-    { label: "Our Service", href: "/about" },
-    { label: "Find a Doctor ", href: "/features" },
-  ];
+  const { header } = data;
 
   return (
     <motion.div
-    initial={{ opacity: 0, y: -50 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-     className="w-full mx-auto bg-white  relative z-50">
-      <Popover className="container mx-auto flex items-center  px-4 py-4  md:max-w-[80rem]">
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full mx-auto bg-white relative z-50"
+    >
+      <Popover className="container mx-auto flex items-center px-4 py-4 md:max-w-[80rem]">
         <Image
-          src="/logo.png"
+          src={header.logo}
           width={132}
           height={50}
           alt="logo"
           className="md:w-[132px] md:h-[50px] w-[100px] h-[40px]"
-        
         />
         <div className="grow">
-          <div className="hidden sm:flex items-center justify-start ml-24 gap-2 md:gap-16 ">
-            {headerLinks.map((link, index) => (
+          <div className="hidden sm:flex items-center justify-start ml-24 gap-2 md:gap-16">
+            {header.navLinks.map((link) => (
               <Link
-                key={index}
+                key={link.label}
                 href={link.href}
                 passHref
-               
                 className="text-[1rem] font-[500] leading-normal hover:text-gray-700"
               >
                 {link.label}
@@ -51,7 +46,6 @@ const Header = () => {
           <Popover.Button
             className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             onClick={() => setMenuOpen(!menuOpen)}
-           
           >
             <span className="sr-only">Open menu</span>
             <Menu className="h-6 w-6" aria-hidden="true" />
@@ -66,7 +60,7 @@ const Header = () => {
           <div className="rounded-lg text-[#646464] bg-white shadow-lg ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50">
             <div className="px-5 pt-5 pb-6">
               <div className="flex items-center justify-between">
-                <Image src="/logo.png" width={83} height={47} alt="logo" />
+                <Image src={header.logo} width={83} height={47} alt="logo" />
                 <div className="mr-2">
                   <Popover.Button
                     className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover-bg-gray-100 hover-text-gray-500 focus-outline-none focus-ring-2 focus-ring-inset focus-ring-indigo-500"
@@ -79,35 +73,35 @@ const Header = () => {
               </div>
               <div className="mt-6">
                 <nav className="grid gap-y-8">
-                  {headerLinks.map((link, index) => (
+                  {header.navLinks.map((link) => (
                     <Link
-                      key={index}
+                      key={link.label}
                       href={link.href}
                       passHref
                       className="focus-outline-none focus-ring-2 focus-ring-inset focus-ring-gray-500 px-2"
+                      onClick={() => setMenuOpen(false)}
                     >
                       {link.label}
                     </Link>
                   ))}
                 </nav>
               </div>
-              {/* mobile screen button */}
               <div className="mt-6 flex flex-col items-center text-center gap-2">
-              <Link href="#contact-section" passHref>
-              <Button className="bg-[#FF7352] md:px-6 md:py-6 px-4 py-4 font-[500] text-[0.93519rem] leading-normal rounded-[10px] w-full">
-            Book now
-          </Button>
-          </Link>
+                <Link href={header.button.href} passHref>
+                  <Button className="bg-[#FF7352] md:px-6 md:py-6 px-4 py-4 font-[500] text-[0.93519rem] leading-normal rounded-[10px] w-full">
+                    {header.button.text}
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-        {/* desktop/tab screen button  */}
+        
         <div className="hidden sm:block">
-        <Link href="#contact-section" passHref>
-          <Button className="bg-[#FF7352] md:px-6 md:py-6 px-4 py-4 font-[500] text-[0.93519rem] leading-normal rounded-[10px]">
-            Book now
-          </Button>
+          <Link href={header.button.href} passHref>
+            <Button className="bg-[#FF7352] md:px-6 md:py-6 px-4 py-4 font-[500] text-[0.93519rem] leading-normal rounded-[10px]">
+              {header.button.text}
+            </Button>
           </Link>
         </div>
       </Popover>
